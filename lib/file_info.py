26a7470e012df09542d9300cb5eab0cc4dd9294e
Alex_mac <huy.trinh43@icloud.com>
2021-01-04 16:59:34 -0800

    testing gitignore

--- lib/file_info.py
+++
@@ -1,5 +0,0 @@
-info = {
-    'class_names': ['TCSS342', 'TCSS371', 'TMATH390'],
-    'source_folder': r'/Users/alextrinh/Downloads',
-    'destination_folder': r'/Users/alextrinh/Library/Mobile Documents/com~apple~CloudDocs/School'
-}
