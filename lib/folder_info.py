f976b0e0bd7cf4529d710f03cfca4367e0091a13
HTrinh43 <55268904+HTrinh43@users.noreply.github.com>
2021-01-04 16:55:01 -0800

    Delete folder_info.py

--- lib/folder_info.py
+++
@@ -1,5 +0,0 @@
-info = {
-    'class_names' : ['TCSS371', 'TCSS342','TMATH390'],
-    'source_folder' : r'/Users/alextrinh/Downloads',
-    'destination_folder': r'/Users/alextrinh/Library/Mobile Documents/com~apple~CloudDocs/School'
-}
