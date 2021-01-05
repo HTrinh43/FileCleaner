e719fd05e2d4b70ce5019153cefc308ead1c98df
Alex_mac <huy.trinh43@icloud.com>
2021-01-04 16:58:30 -0800

    testing git ignore

---
+++ lib/file_info.py
@@ -0,0 +1,5 @@
+info = {
+    'class_names': ['TCSS342', 'TCSS371', 'TMATH390'],
+    'source_folder': r'/Users/alextrinh/Downloads',
+    'destination_folder': r'/Users/alextrinh/Library/Mobile Documents/com~apple~CloudDocs/School'
+}
